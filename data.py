66022f05847aa88484d8e23e8b82b15dc8b01687
AndreyKorovin <martovsky910@gmail.com>
2022-08-10 13:59:48 +0300

    sem8

--- data.py
+++ data.py
@@ -1,7 +1,28 @@
 import input 
 import logger 
+from tkinter import *
 
 def join():
     name, surname, location, number = input.get_phonebook_data()
     logger.append(name, surname, location, number)
     return name, surname, location, number
+
+window = Tk()
+window.title("Телефонный справочник")
+window.geometry('400x250')
+txt = Entry(window,width=10)  
+txt.grid(column=1, row=0) 
+txt = Entry(window,width=10, state='disabled')
+btn = Button(window, text="Загрузить", bg = "grey", fg = "white")
+btn.grid(column = 2, row =0)
+btn = Button(window, text="Поиск", bg = "grey", fg = "white")
+btn.grid(column = 3, row =0)
+btn = Button(window, text="Удалить", bg = "grey", fg = "white")
+btn.grid(column = 4, row =0)
+lbl = Label()
+window.mainloop()
+
+def clicked():  
+    res = "{}".format(txt.get())  
+    lbl.configure(text=res) 
+
--- logger.py
+++ logger.py
@@ -1,8 +1,13 @@
 from input import *
+from datetime import date, datetime as dt
+from time import time
 
 def append(name, surname, location, number): 
+        time = dt.now().strftime('%D - %H:%M')
         with open('log.csv', 'a') as f:
+            f.write(time + '| ')
             f.write(f'{name},{surname},{location},{number}' + '\n') 
+            f.close()
 
 def show():
         with open('log.csv', 'r') as s:
